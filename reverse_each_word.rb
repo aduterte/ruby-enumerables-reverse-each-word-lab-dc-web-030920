b892febddc8d4dd6a5fb6ccbbdecf804dcf99ad2
Alessandro Duterte <alexduterte@gmail.com>
2020-01-31 21:05:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   new = string.split(/ /)
-  new = new.collect{ |word| word.reverse }
-  new = new.join(" ")
+  new = new.collect{ |word| word.reverse }.join(" ")
+
 end
